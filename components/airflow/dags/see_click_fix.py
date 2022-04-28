84cbaed5be36bd2d6634e9dd5f0adbfd5f771f6b
Carl Hinderer <carl.hinderer4@gmail.com>
2022-04-28 16:47:39 -0600

    Added transformations required for inserting into ElasticSearch.

--- components/airflow/dags/see_click_fix.py
+++ components/airflow/dags/see_click_fix.py
@@ -2,7 +2,6 @@ import datetime as dt
 from datetime import timedelta
 import json
 import urllib
-import urllib.request as urllib2
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
@@ -21,13 +20,14 @@ default_args = {
 
 ROOT_DIRECTORY = '/home/carl/Code/Python/crickard-data-engineering'
 ISSUES_FILENAME = ROOT_DIRECTORY + '/output/issues.json'
+TRANSFORMED_FILENAME = ROOT_DIRECTORY + '/output/transformed.json'
 
 
 def downloadIssues():
     try:
         param = {'place_url': 'bernalillo-county', 'per_page': '100'}
         url = 'https://seeclickfix.com/api/v2/issues?' + urllib.parse.urlencode(param)
-        rawreply = urllib2.urlopen(url).read()
+        rawreply = urllib.request.urlopen(url).read()
         reply = json.loads(rawreply)
         with open(ISSUES_FILENAME, 'w') as outfile:
             json.dump(reply, outfile)
@@ -39,6 +39,9 @@ def convertDataForElasticsearch():
     f = open(ISSUES_FILENAME)
     json_data = json.load(f)
     df = pd.DataFrame(json_data['issues'])
+    df['coords'] = df['lat'].astype(str) + ',' + df['lng'].astype(str)
+    df['opendate'] = df['created_at'].str.split('T').str[0]
+    df.to_json(TRANSFORMED_FILENAME)
 
 
 def saveIssuesToElasticsearch():
@@ -53,5 +56,6 @@ with DAG('311DataPipeline',
     getIssues = PythonOperator(task_id='retrieve', python_callable=downloadIssues)
     transformIssues = PythonOperator(task_id='transform', python_callable=convertDataForElasticsearch)
     saveIssues = PythonOperator(task_id='save', python_callable=saveIssuesToElasticsearch)
+
 
 getIssues >> transformIssues >> saveIssues
